951bc3f508a602319ab2363053eff8c600c19206
Manuel Gomez <jmgc@virket.com>
2024-06-12 23:10:50 -0600

    Realizar petición http desde Nest

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
 @Injectable()
 export class SeedService {
 
-  executedSeed() {
-    return `seed exe`;
+  private readonly axios: AxiosInstance = axios;
+
+  async executedSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=5');
+    data.results.forEach( ({name,url})=>{
+      // console.log(name);
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2];
+      console.log({name, no});
+    })
+    return data.results;
   }
 
 }
