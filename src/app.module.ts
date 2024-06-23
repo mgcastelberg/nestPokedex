4b58761de13234f656cc6bb8aaba2a3cff24372c
Manuel Gomez <jmgc@virket.com>
2024-06-23 14:03:54 -0600

    Configuración de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+      ConfigModule.forRoot(),
       ServeStaticModule.forRoot({
         rootPath: join(__dirname,'..','public'),
       }),
-      MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+      MongooseModule.forRoot(process.env.MONGODB),
       PokemonModule,
       CommonModule,
       SeedModule
@@ -19,4 +21,10 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(){
+    console.log(process.env);
+  }
+
+}
